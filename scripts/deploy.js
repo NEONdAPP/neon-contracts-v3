e085139f49e4000fa683354f3ada3880d8de7e95
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2023-01-21 14:46:29 +0100

    Prj config + added NPairs_out
    - Project configurations
    - Added NPaits_out first version

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const Token = await ethers.getContractFactory("NPairs_out");
+  const token = await Token.deploy(deployer.address);
+
+  console.log("NPairs-out address:", token.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
