9d207cb3cfc06c780b70a3d6a28881f6f4b660c1
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2023-02-06 17:57:45 +0100

    Complete NHistorian + Added NDCA
    - Completed NHistorian Testing + NatSpec
    - Added base concept NDCA

--- contracts/NHistorian.sol
+++ contracts/NHistorian.sol
@@ -4,55 +4,56 @@ pragma solidity 0.8.17;
 contract NHistorian {
 
     struct data{
-        mapping (uint256 => detail) userData;
+        mapping (uint256 => histDetail) userData;
         uint8 totStored;
         uint8 bufferId;
     }
 
-    struct detail{
+    struct histDetail{
+        address srcToken;
         uint256 chainId;
         address destToken;
         uint256 closedDcaTime;
-        uint8 reason; // (0 = Completed, 1 = User Close DCA, 2 = Insufficient User Approval or Balance)
+        uint8 reason; // (0 = Completed, 1 = User Close DCA, ...)
     }
 
     mapping (address => data) database;
 
     /* WRITE METHODS*/
-
-    function storeDCA(address _userAddress, detail calldata _struct) internal {
-        require(_userAddress != address(0), "NHistorian: null address not allowed");
+    /**
+     * @notice  store DCA data to buffer database.
+     * @param   _userAddress  reference address of the owner.
+     * @param   _struct  data to be stored.
+     */
+    function storeDCA(address _userAddress, histDetail calldata _struct) internal {
+        require(_userAddress != address(0), "NHistorian: Null address not allowed");
         //buffer
-        database[_userAddress].bufferId = database[_userAddress].totStored >= 200 ? 1 : database[_userAddress].bufferId ++;
+        database[_userAddress].bufferId = database[_userAddress].bufferId >= 200 ? 1 : database[_userAddress].bufferId +1;
         uint8 bufferId = database[_userAddress].bufferId;
+        database[_userAddress].userData[bufferId].srcToken = _struct.srcToken;
         database[_userAddress].userData[bufferId].chainId = _struct.chainId;
         database[_userAddress].userData[bufferId].destToken = _struct.destToken;
         database[_userAddress].userData[bufferId].closedDcaTime = _struct.closedDcaTime > 0 ? _struct.closedDcaTime : block.timestamp;//Manage case of DCA closed without exe
         database[_userAddress].userData[bufferId].reason = _struct.reason;
-        unchecked {
-            database[_userAddress].totStored ++;
+        if(database[_userAddress].totStored < 200){
+            unchecked {
+                database[_userAddress].totStored ++;
+            }
         }
     }
-        
-
     /* VIEW METHODS*/
-
-
-    function getHistoryDataBatch(address _userAddress) internal view returns(detailStruct[] memory){
-        dataStruct storage data = database[_userAddress];
-        uint256 storeID = data.storeID;
-        detailStruct[] memory dataOut = new detailStruct[](storeID);
-        for(uint256 i=1; i<=storeID; i++){
-            dataOut[i-1] = data.userData[i];
+    /**
+     * @notice  Retrieve all data from a specific address.
+     * @param   _userAddress  reference address.
+     * @return  histDetail batch data for each nBatch.
+     * @return  nBatch number of batch data retrieved.
+     */
+    function getHistoryDataBatch(address _userAddress) internal view returns(histDetail[] memory, uint8 nBatch){
+        uint8 totStored = database[_userAddress].totStored;
+        histDetail[] memory dataOut = new histDetail[](totStored);
+        for(uint8 i=1; i<=totStored; i++){
+            dataOut[i-1] = database[_userAddress].userData[i];
         }
-        return dataOut;
-    }
-
-    function getHistoryData(address _userAddress, uint256 _storeId) internal view returns(uint256 pairId, uint256 closedDcaTime, uint256 destTokenEarned, uint reason){
-        dataStruct storage data = database[_userAddress];
-        pairId = data.userData[_storeId].pairId;
-        closedDcaTime = data.userData[_storeId].closedDcaTime;
-        destTokenEarned = data.userData[_storeId].destTokenEarned;
-        reason = data.userData[_storeId].reason;
+        return (dataOut, totStored);
     }
 }
--- contracts/test/TestNHistorian.sol
+++ contracts/test/TestNHistorian.sol
@@ -5,4 +5,11 @@ import "../NHistorian.sol";
 
 contract TestNHistorian is NHistorian {
 
+    function testStore(address _userAddress, histDetail calldata _struct) external{
+        storeDCA(_userAddress, _struct);
+    }
+
+    function testGetData(address _userAddress) external view returns(histDetail[] memory, uint8 nBatchData){
+        return(getHistoryDataBatch(_userAddress));
+    }
 }
