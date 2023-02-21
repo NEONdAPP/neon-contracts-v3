eae0bbf7b4672a7c12401d59751a472e0d54158a
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2023-02-21 16:30:55 +0100

    adjust names

--- contracts/test/TestNHistorian.sol
+++ contracts/test/TestNHistorian.sol
@@ -5,11 +5,11 @@ import "../NHistorian.sol";
 
 contract TestNHistorian is NHistorian {
 
-    function testStore(address _userAddress, histDetail calldata _struct) external{
+    function store(address _userAddress, histDetail calldata _struct) external{
         _storeDCA(_userAddress, _struct);
     }
 
-    function testGetData(address _userAddress) external view returns(histDetail[] memory, uint8 nBatchData){
+    function gestData(address _userAddress) external view returns(histDetail[] memory, uint8 nBatchData){
         return(_getHistoryDataBatch(_userAddress));
     }
 }
--- test/NHistorian.js
+++ test/NHistorian.js
@@ -17,40 +17,40 @@ describe("NHistorian Testing", function () {
             const { contract, owner, addr1 } = await loadFixture(deployContract);
 
             await expect(
-                contract.connect(owner).testStore(ethers.constants.AddressZero, [addr1.address, 1, addr1.address, 69, 4])
+                contract.connect(owner).store(ethers.constants.AddressZero, [addr1.address, 1, addr1.address, 69, 4])
             ).to.be.revertedWith("NHistorian: Null address not allowed");
         });
     //Correct Events
         it("Should increase data counter by 1", async function () {
             const { contract, owner, addr1 } = await loadFixture(deployContract);
-            await contract.connect(owner).testStore(owner.address, [addr1.address, 1, addr1.address, 69, 4]);
-            var result = await contract.connect(owner).testGetData(owner.address);
+            await contract.connect(owner).store(owner.address, [addr1.address, 1, addr1.address, 69, 4]);
+            var result = await contract.connect(owner).getData(owner.address);
             expect(result[1]).to.equal(1);
         });
 
         it("Should be limited at 200", async function () {
             const { contract, owner, addr1 } = await loadFixture(deployContract);
             for(var i=1; i<=201; i++){
-                await contract.connect(owner).testStore(owner.address, [addr1.address, 1, addr1.address, 69, 4]);
+                await contract.connect(owner).store(owner.address, [addr1.address, 1, addr1.address, 69, 4]);
             }
-            var result = await contract.connect(owner).testGetData(owner.address);
+            var result = await contract.connect(owner).getData(owner.address);
             expect(result[1]).to.equal(200);
         });
 
         it("If Addr1 store data, Addr2 should remains unchanged", async function () {
             const { contract, owner, addr1, addr2 } = await loadFixture(deployContract);
-            await contract.connect(owner).testStore(addr1.address, [addr1.address, 1, addr1.address, 69, 4]);
-            var result = await contract.connect(owner).testGetData(addr2.address);
+            await contract.connect(owner).store(addr1.address, [addr1.address, 1, addr1.address, 69, 4]);
+            var result = await contract.connect(owner).getData(addr2.address);
             expect(result[1]).to.equal(0);
         });
 
         it("Over 200 should restart from the first one", async function () {
             const { contract, owner, addr1 } = await loadFixture(deployContract);
             for(var i=1; i<=200; i++){
-                await contract.connect(owner).testStore(owner.address, [addr1.address, 1, addr1.address, 69, 4]);
+                await contract.connect(owner).store(owner.address, [addr1.address, 1, addr1.address, 69, 4]);
             }
-            await contract.connect(owner).testStore(owner.address, [addr1.address, 69, addr1.address, 69, 69]);
-            var result = await contract.connect(owner).testGetData(owner.address);
+            await contract.connect(owner).store(owner.address, [addr1.address, 69, addr1.address, 69, 69]);
+            var result = await contract.connect(owner).getData(owner.address);
             expect(result[0][0][1]).to.equal(ethers.BigNumber.from(69));//chainId
         });
     });
