83ae382e56791605f022efc9ab76b32115f8de1c
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2023-03-07 20:30:09 +0100

    Updated Simulate Strategy
    - Add simulation strategy for testnet
    - Completed test script

--- contracts/interfaces/IBeefy.sol
+++ contracts/interfaces/IBeefy.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 interface IBeefyVault {
-    //Polygon, Arbitrum, Optimism, Gnosis
     function deposit(uint256 _amount) external;
     function balanceOf(address _account) external view returns (uint256);//receipt
     function transfer(address _recipient, uint256 _amount) external returns (bool);
--- contracts/interfaces/ICurveFi.sol
+++ contracts/interfaces/ICurveFi.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 interface ICurveFi_pool2T {
-    //Polygon, Arbitrum, Optimism, Gnosis
     function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount) external returns (uint256);   
     function coins(uint256 _arg) external view returns (address);//ref Token
 }
