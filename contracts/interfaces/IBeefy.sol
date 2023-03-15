b6bfe99481148bec5b5acabb967ecebfdd57cd33
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2023-03-15 22:36:26 +0100

    Beefy & Curve
    - Updated ref names

--- contracts/interfaces/IBeefy.sol
+++ contracts/interfaces/IBeefy.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface IBeefyVault {
+interface IBeefyVault_2T {
     function deposit(uint256 _amount) external;
     function balanceOf(address _account) external view returns (uint256);//receipt
     function transfer(address _recipient, uint256 _amount) external returns (bool);
--- contracts/interfaces/ICurveFi.sol
+++ contracts/interfaces/ICurveFi.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-interface ICurveFi_pool2T {
+interface ICurveFi_lp2T {
     function add_liquidity(uint256[2] calldata amounts, uint256 min_mint_amount) external returns (uint256);   
     function coins(uint256 _arg) external view returns (address);//ref Token
 }
