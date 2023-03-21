09018f1d06160c377e88ee92d6f0b9b66544e809
Hyper0x0 <122046226+Hyper0x0@users.noreply.github.com>
2023-03-21 22:03:43 +0100

    Added flat files for verification

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,10 +3,20 @@ async function main() {
 
   console.log("Deploying contracts with the account:", deployer.address);
 
-  const contract = await ethers.getContractFactory("NPairs");
-  const NPairs = await contract.deploy();
+  const params = {
+      resolver: "0xc341B6b1C2A2017AFD9686aA879FfFE84E21d8eD",
+      defaultApproval: 1500000000, //"1.500.000.000"
+      timeBase: 86400,
+      minTau: 1,
+      maxTau: 30
+  }
 
-  console.log("NPairs address:", NPairs.address);
+  const contractFactory = await ethers.getContractFactory("NCore");
+  const contract = await contractFactory.deploy(params.resolver, params.defaultApproval, params.timeBase, params.minTau, params.maxTau);
+
+  console.log("NCore address:", contract.address);
+  console.log("NDCA address:", await contract.DCA());
+  console.log("NPairs address:", await contract.POOL());
 }
 
 // We recommend this pattern to be able to use async/await everywhere
